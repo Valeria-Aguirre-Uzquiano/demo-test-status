993c8adf6e105cca6670f0677f0ee7e8b661742c
marioly7 <mariolyv7@gmail.com>
2023-11-01 12:42:18 -0400

    Adding profiles

--- demo/src/main/java/com/example/demo/serviceImplement/ProfileServiceImplement.java
+++ demo/src/main/java/com/example/demo/serviceImplement/ProfileServiceImplement.java
@@ -17,14 +17,29 @@ public class ProfileServiceImplement implements ProfileService {
             return Arrays.asList(
                     new Profile("Alice", "Williams", "Bolivia"),
                     new Profile("Bob", "Marley", "Bolivia"),
-                    new Profile("Alex", "Martinez", "Bolivia")
+                    new Profile("Alex", "Martinez", "Bolivia"),
+                    new Profile("Raul", "Gonzales", "Bolivia"),
+                    new Profile("Maria", "Garcia", "Bolivia"),
+                    new Profile("Juan", "Perez", "Bolivia"),
+                    new Profile("Carlos", "Gutierrez", "Bolivia"),
+                    new Profile("Luis", "Rodriguez", "Bolivia"),
+                    new Profile("Jose", "Gomez", "Bolivia"),
+                    new Profile("Ana", "Hernandez", "Bolivia"),
+                    new Profile("Miguel", "Sanchez", "Bolivia"),
+                    new Profile("Elena", "Diaz", "Bolivia"),
+                    new Profile("Pablo", "Torres", "Bolivia"),
+                    new Profile("Sofia", "Flores", "Bolivia")
             );
         }else{
             return Arrays.asList(
                     new Profile("Charlie", "Garcia", "Argentina"),
                     new Profile("David", "Lee", "Argentina"),
                     new Profile("Frank", "Lambert", "Argentina"),
-                    new Profile("John", "Doe", "Argentina")
+                    new Profile("John", "Doe", "Argentina"),
+                    new Profile("Mike", "Smith", "Argentina"),
+                    new Profile("Steve", "Brown", "Argentina"),
+                    new Profile("Mary", "Johnson", "Argentina"),
+                    new Profile("Kate", "Doe", "Argentina")
             );
         }
     }
@@ -32,17 +47,33 @@ public class ProfileServiceImplement implements ProfileService {
     @Override
     public List<Profile> getAllProfiles() {
         return Arrays.asList(
+                new Profile("Mary", "Johnson", "Peru"),
+                new Profile("Kate", "Doe", "Peru"),
                 new Profile("Alice", "Williams", "Bolivia"),
                 new Profile("Bob", "Marley", "Bolivia"),
+                new Profile("Alex", "Martinez", "Bolivia"),
+                new Profile("Raul", "Gonzales", "Bolivia"),
+                new Profile("Maria", "Garcia", "Bolivia"),
+                new Profile("Juan", "Perez", "Bolivia"),
+                new Profile("Carlos", "Gutierrez", "Bolivia"),
+                new Profile("Luis", "Rodriguez", "Bolivia"),
+                new Profile("Jose", "Gomez", "Bolivia"),
+                new Profile("Ana", "Hernandez", "Bolivia"),
+                new Profile("Mike", "Smith", "Chile"),
+                new Profile("Miguel", "Sanchez", "Bolivia"),
+                new Profile("Elena", "Diaz", "Bolivia"),
+                new Profile("Pablo", "Torres", "Bolivia"),
+                new Profile("Sofia", "Flores", "Bolivia"),
+                new Profile("Molly", "Allen", "Peru"),
                 new Profile("Charlie", "Garcia", "Argentina"),
                 new Profile("David", "Lee", "Argentina"),
                 new Profile("Frank", "Lambert", "Argentina"),
                 new Profile("John", "Doe", "Argentina"),
-                new Profile("Mike", "Smith", "Chile"),
                 new Profile("Steve", "Brown", "Chile"),
-                new Profile("Mary", "Johnson", "Peru"),
-                new Profile("Kate", "Doe", "Peru"),
-                new Profile("Alex", "Martinez", "Bolivia")
+                new Profile("Mike", "Smith", "Argentina"),
+                new Profile("Steve", "Brown", "Argentina"),
+                new Profile("Mary", "Johnson", "Argentina"),
+                new Profile("Kate", "Doe", "Argentina")
         );
     }
 }
